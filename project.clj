26ca69d0276c1e1e59ce653d5aa1206087ddec25
Walter P Babic <wbabic@mac.com>
2015-07-14 00:31:59 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject complex "0.1.0"
+  :description "Library for complex numbers. geometry and transforms"
+  :url "http://wbabic.github.io"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/test.check "0.7.0"]
+                 [prismatic/schema "0.4.3"]
+                 [com.cognitect/transit-cljs "0.8.220"]])
